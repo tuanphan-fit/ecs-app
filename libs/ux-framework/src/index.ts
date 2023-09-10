4f990d107dbafc633daf44c39b062770064d3d1c
Tuan Phan <tuanphan.fit@gmail.com>
2023-09-10 23:52:08 +0700

    fix export lib

--- libs/ux-framework/src/index.ts
+++ libs/ux-framework/src/index.ts
@@ -1,2 +1,6 @@
+// module exports
 export * from './lib/ux-framework.module';
 export * from './lib/layout/layout.module';
+
+// component exports
+export * from './lib/layout';
---
+++ libs/ux-framework/src/lib/layout/index.ts
@@ -0,0 +1,2 @@
+export * from './default-layout/ecs-default-layout.component';
+export * from './header/ecs-b2e-header.component';
